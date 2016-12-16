0fa5f77f5ddd49ac3d3b9a69548a954819ca6822
0910 <e@nuevediez.com.ar>
2016-12-15 21:00:23 -0300

    change contact enhacement

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,10 +8,8 @@ class ContactsController < ApplicationController
     @contact.request = request
     if @contact.deliver
       flash.now[:notice] = 'Gracias por tu mensaje. Pronto nos pondremos en contacto.'
-      render 'home/index'
     else
       flash.now[:error] = 'El envío del mensaje ha fallado. Inténtalo nuevamente.'
-      render 'home/index'
     end
   end
 end
